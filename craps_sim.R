4ce24bfe8fd2935ea4fca91d0386a34d81c40d9e
jmercer187 <jake.mercer@gmail.com>
2019-06-06 20:51:26 -0400

    finished game program with 1 place and 1 pass bet; added loop to iterate through n games and histograms of results

--- craps_sim.R
+++ craps_sim.R
@@ -1,7 +1,14 @@
+library(tidyverse)
+
+
+
+#   ROLL DIE FUNC
 
 RollDie <- function(n) {
   sample(1:6,n, replace = T)
 }
+
+#   COME OUT FUNC
 
 ComeOutBet <- function(result, bet, bet_type) {
   
@@ -20,6 +27,8 @@ ComeOutBet <- function(result, bet, bet_type) {
   return(bet)  
 }
 
+#   POINT BET FUNC
+
 PointBet <- function(result, point, bet, bet_type) {
   
   if (result == point & bet_type == "pass"){
@@ -35,15 +44,16 @@ PointBet <- function(result, point, bet, bet_type) {
   return(bet)
 }
 
+#   PASS BET FUNC
 
-PassBet <- function(result, bet, bet_number, fee){
+PassBet <- function(result, bet, bet_number, vig){
   if (result == 7){
     if (bet_number == 4 | bet_number == 10){
-      bet = (bet * 2) - fee
+      bet = (bet * 2) - ((bet * 2) * vig)
     } else if (bet_number == 5 | bet_number == 9){
-      bet = (bet * (2/3)) - fee
+      bet = (bet * (2/3)) - ((bet * (2/3)) * vig)
     } else if (bet_number == 6 | bet_number == 8){
-      bet = (bet * (5/6)) - fee
+      bet = (bet * (5/6)) - ((bet * (5/6)) * vig)
     }
   } else if (result == bet_number){
     bet = 0
@@ -51,17 +61,34 @@ PassBet <- function(result, bet, bet_number, fee){
   
   return(bet)
 }
-  
-#PlaceBet
 
+#   PLACE BET FUNC
+
+PlaceBet <- function(result, bet, bet_number, vig){
+
+  if (result == 7){
+    bet = 0
+  } else if (result == 4 | result == 10){
+    bet = (bet * (9/5)) - ((bet * (9/5)) * vig)
+  } else if (result == 5 | result == 9){
+    bet = (bet * (7/5)) - ((bet * (7/5)) * vig)
+  } else if (result == 6 | result == 8){
+    bet = (bet * (7/6)) - ((bet * (7/6)) * vig)
+  } 
+  
+  return(bet)
+}
+
+#   PLAY GAME FUNC
 
 PlayGame <- function(bet, bet_type,
-                     psb=NULL, psb_number=NULL, psb_fee=NULL, 
-                     place_bet1 = NULL, place_bet1_number=NULL
+                     psb=NULL, psb_number=NULL, psb_vig=NULL, 
+                     plcb = NULL, plcb_number=NULL, plcb_vig=NULL
                      ){
   
   base_bets <- vector (mode = 'integer')
   psb_winnings <- vector(mode = "integer")
+  plcb_winnings <- vector(mode = "integer")
   
   bet_type = 'dont pass'
   
@@ -74,13 +101,22 @@ PlayGame <- function(bet, bet_type,
   
   bet <- ComeOutBet(result, bet, bet_type)
   
-  psb_outcome <- PassBet(result, psb, psb_number, psb_fee)
+  psb_outcome <- PassBet(result, psb, psb_number, psb_vig)
   
   if (psb_outcome > 0){
     psb_winnings <- append(psb_winnings, psb_outcome)
   } else if (psb_outcome == 0){
     psb <- 0
   }
+  
+  plcb_outcome <- PlaceBet(result, plcb, plcb_number, plcb_vig)
+  
+  if (plcb_outcome > 0){
+    plcb_winnings <- append(plcb_winnings, plcb_outcome)
+  } else if (plcb_outcome == 0){
+    plcb <- 0
+  }
+  
   
   if (bet_init == bet){
     point <- result
@@ -89,12 +125,22 @@ PlayGame <- function(bet, bet_type,
     print(cat("point 1 :", roll, point, sep = " "))
     
     if (psb != 0){
-      psb_outcome <- PassBet(result, psb, psb_number, psb_fee)
+      psb_outcome <- PassBet(result, psb, psb_number, psb_vig)
       
       if (psb_outcome > 0){
         psb_winnings <- append(psb_winnings, psb_outcome)
       } else if (psb_outcome == 0){
         psb <- 0
+      }
+    }
+    
+    if (plcb != 0){
+      plcb_outcome <- PlaceBet(result, plcb, plcb_number, plcb_vig)
+      
+      if (plcb_outcome > 0){
+        plcb_winnings <- append(plcb_winnings, plcb_outcome)
+      } else if (plcb_outcome == 0){
+        plcb <- 0
       }
     }
     
@@ -107,7 +153,7 @@ PlayGame <- function(bet, bet_type,
         result <- sum(roll)
         
         if (psb != 0){
-          psb_outcome <- PassBet(result, psb, psb_number, psb_fee)
+          psb_outcome <- PassBet(result, psb, psb_number, psb_vig)
           
           if (psb_outcome > 0){
             psb_winnings <- append(psb_winnings, psb_outcome)
@@ -116,7 +162,16 @@ PlayGame <- function(bet, bet_type,
           }
         }
         
-        print(result)
+        if (plcb != 0){
+          plcb_outcome <- PlaceBet(result, plcb, plcb_number, plcb_vig)
+          
+          if (plcb_outcome > 0){
+            plcb_winnings <- append(plcb_winnings, plcb_outcome)
+          } else if (plcb_outcome == 0){
+            plcb <- 0
+          }
+        }
+        
       }
       
       bet <- PointBet(result, point, bet, bet_type)
@@ -132,9 +187,48 @@ PlayGame <- function(bet, bet_type,
   return(bet_outcomes)
 }
 
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+bet = 20
+bet_type = "pass"
+place_bet = 0
+place_bet_number = 0
+place_bet_vig = .05
+pass_bet = 35
+pass_bet_number = 6
+pass_bet_vig = .05
+iterations = 1000
+
+game_walk <- vector(mode = "integer")
+
+for (i in seq_along(1:iterations)){
+  game_result <- PlayGame(bet, bet_type, place_bet, place_bet_number, place_bet_vig, pass_bet, pass_bet_number, pass_bet_vig)
+  
+  game_walk <- append(game_walk, game_result)
+}
+
+game_walk_df <- as.tibble(game_walk)
+
+# ggplot(data = game_walk_df) +
+#   geom_histogram(aes(x = value), binwidth = 6) +
+#   geom_vline(xintercept = 85) + 
+#   ggtitle("Game Result",
+#           subtitle = "Don't Pass = 20, Place on 9 = 30, Pass on 6 = 35")
+
+# ggplot(data = game_walk_df) +
+#   geom_histogram(aes(x = value), binwidth = 6) +
+#   geom_vline(xintercept = 20) + 
+#   ggtitle("Game Result",
+#           subtitle = "Don't Pass = 20")
+
+ggplot(data = game_walk_df) +
+  geom_histogram(aes(x = value), binwidth = 6) +
+  geom_vline(xintercept = 20) +
+  ggtitle("Game Result",
+          subtitle = "Pass = 20")
 
 
 
